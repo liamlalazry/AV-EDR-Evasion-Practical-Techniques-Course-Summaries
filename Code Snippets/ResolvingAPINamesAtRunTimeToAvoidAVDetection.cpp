605b32203d0497a774005b9a616c800c0098ca1f
liamlalazry <liamlalasri@gmail.com>
2026-07-25 20:08:29 +0300

    Add runtime API resolution to avoid AV detection

---
+++ Code Snippets/ResolvingAPINamesAtRunTimeToAvoidAVDetection.cpp
@@ -0,0 +1,11 @@
+FARPROC get_api(const char* lib, const char* func) {
+    HMODULE hMod = LoadLibraryA(lib);  // Load the DLL
+    return GetProcAddress(hMod, func); // Resolve function address
+}
+typedef LPVOID(WINAPI* pVirtualAlloc)(LPVOID, SIZE_T, DWORD, DWORD);
+
+int main() {
+    pVirtualAlloc myVA = (pVirtualAlloc)get_api("kernel32.dll", "VirtualAlloc");
+    void* mem = myVA(NULL, 4096, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    return 0;
+}
